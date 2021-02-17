ae2d30bb897d6095263bcaa1c75d645c6ff1572a
id872 <45285868+id872@users.noreply.github.com>
2021-02-17 21:58:53 +0100

    streamable-video-downloader - initial revision

---
+++ README.md
@@ -0,0 +1,8 @@
+# streamable-video-downloader
+Python script for streamable video download.
+
+# usage
+```shell
+python3 streamable_video_downloader.py https://streamable.com/rvq5jy
+```
+rvq5jy.mp4 file will be downloaded.
---
+++ streamable_video_downloader.py
@@ -0,0 +1,64 @@
+import logging
+from argparse import ArgumentParser
+from urllib import request
+
+
+class ExecutionError(Exception):
+    pass
+
+
+def download_video_file_decorator(func_decorated):
+    def func_wrapper(*args, **kwargs):
+        direct_video_url = func_decorated(*args, **kwargs)
+
+        if not direct_video_url or 'https://' not in direct_video_url:
+            raise ExecutionError('Video URL not found')
+
+        patterns = '/video/mp4/', '?'
+        file_name = direct_video_url.split(patterns[0])[1].split(patterns[1])[0] \
+            if patterns[0] in direct_video_url else 'video.mp4'
+
+        logging.info('Direct link:\n%s\nDownloading...', direct_video_url)
+        with open(file_name, 'wb') as video_file:
+            video_file.write(request.urlopen(direct_video_url).read())
+        logging.info('Video downloaded')
+
+    return func_wrapper
+
+
+def video_url_getter_decorator(fun_decorated):
+    def func_wrapper(*args, **kwargs):
+        html_content = fun_decorated(*args, **kwargs)
+        patterns = '<meta property="og:video:url" content="', '">'
+        return html_content.split(patterns[0])[1].split(patterns[1])[0] \
+            if patterns[0] in html_content else None
+
+    return func_wrapper
+
+
+@download_video_file_decorator
+@video_url_getter_decorator
+def streamable_video_download(streamable_link):
+    try:
+        logging.info('Downloading video from: %s', streamable_link)
+        return str(request.urlopen(streamable_link).read())
+    except Exception as exc:
+        raise ExecutionError('Error: could not download data from {}'.format(streamable_link)) \
+            from exc
+
+
+def main():
+    logging.basicConfig(format="%(asctime)s-%(levelname)s -- %(message)s", level=logging.INFO)
+
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument(dest='streamable_url', help="URL to streamable video")
+    args = arg_parser.parse_args()
+
+    try:
+        streamable_video_download(args.streamable_url)
+    except ExecutionError as ex_err:
+        logging.error(ex_err)
+
+
+if __name__ == "__main__":
+    main()
